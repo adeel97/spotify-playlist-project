555b2cd30a05aca907fe0b49c9ba5d1b30ca741d
Adeel Shaikh <adeelshaikh@Adeels-Mac-mini.attlocal.net>
2021-02-21 16:05:40 -0600

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('cookie-session');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const csurf = require('csurf');
+const dotenv = require('dotenv');
+const path = require('path');
+
+
+/* Import config */
+dotenv.config({path: path.resolve(__dirname, '.env')});
+
+/* Create Express App */
+const app = express();
+
+/* Set Security Configs */
+app.use(helmet());
+app.use(hpp());
+
+/* Set Cookie Settings */
+app.use(
+    session({
+        name: 'session',
+        secret: process.env.COOKIE_SECRET,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
+    })
+);
+app.use(csurf());
+
+
+app.listen(8080, () => {
+    console.log("I'm listening!");
+});
+
+module.exports = app;
